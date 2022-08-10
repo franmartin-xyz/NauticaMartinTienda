31ceefdc0de061bb3d5314ce99bf1b33fefb0258
Francisco Martin <franmartin241001@gmail.com>
2022-08-09 23:48:03 -0300

    404 redirect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./App.css"
 import {Navbar,CartContainer,ItemListContainer} from "./components"
 import {Footer, Header, ItemDetailContainer} from "./containers"
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Redirect} from "react-router-dom"
 import { CartContext } from './context/CartContext'
 import LoginContext from './context/LoginContext'
 import { ModalProvider } from "styled-react-modal"
